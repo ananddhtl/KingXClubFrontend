0bf473f4e28123d7cecc70268c93a47dc342adf6
Bibek <zhabibek4u@gmail.com>
2024-05-11 15:14:43 +0530

    bottom nav icon size reduced

--- src/components/DrawerNav/BottomNavbar.jsx
+++ src/components/DrawerNav/BottomNavbar.jsx
@@ -6,29 +6,29 @@ function BottomNavbar() {
     return (
         <div>
             <div className="fixed w-full justify-center  bottom-0 z-10 left-0 right-0 bg-transparent backdrop-blur-lg md:hidden">
-                <nav className="flex h-auto w-full justify-around items-center ">
-                <NavLink className="flex justify-center h-[80px] w-[43px] flex-col hover:text-orange-500" to="/games">
+                <nav className="flex h-16 w-full justify-around items-center ">
+                <NavLink className="flex justify-center h-[70px] w-[35px] flex-col hover:text-orange-500" to="/games">
                         <img className="" src= "./assets/img/games.png"/>
-                        <p className='text-sm -mt-2 font-serif text-center'>Games</p>
+                        <p className='text-xs -mt-2 font-mono text-center'>Games</p>
                     </NavLink>
 
-                    <NavLink className="flex justify-center h-[80px] w-[43px] flex-col hover:text-orange-500" to="/bids">
+                    <NavLink className="flex justify-center h-[70px] w-[35px] flex-col hover:text-orange-500" to="/bids">
                         <img className="" src= "./assets/img/bids.png"/>
-                        <p className='text-sm font-serif -mt-2 text-center'>Bids</p>
+                        <p className='text-xs -mt-2 font-mono text-center'>Bids</p>
                     </NavLink>
 
-                    <NavLink className=" flex justify-center h-[75px] items-start" to="https://wa.me/+971563664115">
-                        <img className='h-[75px] w-[75px]' src= "./assets/img/contact.png"/>
+                    <NavLink className=" flex justify-center h-[75px] items-center" to="https://wa.me/+971563664115">
+                        <img className='h-[60px] w-[60px]' src= "./assets/img/contact.png"/>
                     </NavLink>
 
-                    <NavLink className="flex justify-center h-[80px] w-[43px] flex-col hover:text-orange-500" to="/result">
+                    <NavLink className="flex justify-center h-[70px] w-[35px] flex-col hover:text-orange-500" to="/result">
                         <img className="" src= "./assets/img/result.png"/>
-                        <p className='text-sm font-serif -mt-2 text-center'>Result</p>
+                        <p className='text-xs -mt-2 font-mono text-center'>Result</p>
                     </NavLink>
 
-                    <NavLink className="flex justify-center h-[80px] w-[43px] flex-col hover:text-orange-500" to="https://wa.me/+971563664115">
-                        <img className="" src= "./assets/img/support.png"/>
-                        <p className='text-sm font-serif -mt-2 text-center'>Support</p>
+                    <NavLink className="flex justify-center h-[70px] w-[50px] flex-col  hover:text-orange-500" to="https://wa.me/+971563664115">
+                        <img className=" w-[35px] justify-center ml-2" src= "./assets/img/support.png"/>
+                        <p className='text-xs -mt-2 font-mono text-center'>Support</p>
                     </NavLink>
                 </nav>
             </div>
